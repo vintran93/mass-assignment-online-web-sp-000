618e81184c875815c672d1f1c281bf0eeec2f530
Vincent Tran <vincentuong@yahoo.com>
2021-06-29 02:51:54 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -11,10 +11,3 @@ end
 
 
 
-class User
-  attr_accessor :name, :user_name, :age, :location, :bio
- 
-  def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
-  end
-end
